03e4d4fd46aa56092c823530d70e4abe5f0d59d5
Ryan Harris <ryan.t.harris67@gmail.com>
2022-12-06 13:49:37 -0600

    Button component deleted & Landing page updated.

--- lockerrm-front/src/components/Landing.js
+++ lockerrm-front/src/components/Landing.js
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types'
-import Button from './Button'
 
 const Landing = ( props, {} ) => {
     const onClick = (e) => {
@@ -8,39 +7,134 @@ const Landing = ( props, {} ) => {
     }
 
     return (
-        <div className="landing">
-            <form action="#">
 
-                <div className="loginForm" style={loginStyle}>
-                    <input type="text" className="username" placeholder="Username"/>
-                    <label htmlFor="username"></label>
+        <div className="landingPage" style={landingPage}>
 
-                    <input type="password" className="password" placeholder="Password"/>
-                    <label htmlFor="password"></label>
+            <div className="landingInfo" style={landingInfo}>
+                <h1 style={header}>{props.header}</h1>
+                <p style={message}>{props.message}</p>
+            </div>
 
-                    <Button text="Log-In" onClick={onClick} />
-                    <Button text="Create New Account" onClick={onClick} />
-                </div>
+            <div className="landingForm" style={landingForm}>
+                <input type="text" className="username" placeholder="Username" style={usernameForm}/>
 
-            </form>
+                <input type="password" className="password" placeholder="Password" style={passwordForm}/>
+
+                <button style={loginBtn} onClick={onClick}>Log-In</button>
+
+                <a href="#">Forgot Password?</a>
+
+                <button style={registerBtn} onClick={onClick}>Create New Account?</button>
+            </div>
+
         </div>
+
     )
 }
 
 Landing.defaultProps = {
-    message: 'Main Welcome message...(x)'
+    header: 'Main Welcome Header...(x)',
+    message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et maxime quo voluptas consectetur corporis, mollitia reprehenderit eveniet totam expedita impedit voluptate asperiores accusamus suscipit doloribus fugit deserunt placeat ipsa amet!',
 }
 
 Landing.propTypes = {
     message: PropTypes.string.isRequired
 }
 
-const loginStyle = {
-    margin: 'auto',
-    backgroundColor: 'inherit',
-    width: '50vw',
-    height: '50vh',
+//Landing Page styling 
+const landingPage = {
     display: 'flex',
+    margin: 'auto',
+    width: '95vw',
+    height: '95vh',
+}
+
+//Landing Info styling
+const landingInfo = {
+    display: 'grid',
+    margin: 'auto',
+    border: 'solid black',
+    borderRadius: '15px',
+    width: '40%',
+    height: '60%'
+}
+const header = {
+    gridColumn: 1,
+    gridRow: 1,
+    margin: 'auto',
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    justifyItems: 'center',
+    alignItems: 'center',
+}
+const message = {
+    gridColumn: 1,
+    gridRow: 2,
+    margin: 'auto',
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    justifyItems: 'center',
+    alignItems: 'center',
+}
+
+//Landing Form styling
+const landingForm = { 
+    display: 'grid',
+    margin: 'auto',
+    border: 'solid black',
+    borderRadius: '15px',
+    width: '40%',
+    height: '60%',
+}
+const usernameForm = {
+    gridColumn: 1,
+    gridRow: 1,
+    margin: 'auto',
+    width: '85%',
+    height: '50%',
+    display: 'flex',
+    justifyContent: 'center',
+    justifyItems: 'center',
+    borderRadius: '10px',
+}
+const passwordForm = {
+    gridColumn: 1,
+    gridRow: 2,
+    margin: 'auto',
+    width: '85%',
+    height: '50%',
+    display: 'flex',
+    justifyContent: 'center',
+    justifyItems: 'center',
+    borderRadius: '10px'
+}
+const loginBtn = {
+    gridColumn: 1,
+    gridRow: 3,
+    margin: 'auto',
+    width: '85%',
+    height: '50%',
+    display: 'flex',
+    justifyContent: 'center',
+    justifyItems: 'center',
+    alignItems: 'center',
+    borderRadius: '10px'
+}
+const registerBtn = {
+    gridColumn: 1,
+    gridRow: 4,
+    margin: 'auto',
+    width: '50%',
+    height: '50%',
+    display: 'flex',
+    justifyContent: 'center',
+    justifyItems: 'center',
+    alignItems: 'center',
+    borderRadius: '10px'
 }
 
 export default Landing
