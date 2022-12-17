fb945c2e58403dad8461ee061890fdafe8be6935
Ryan Harris <ryan.t.harris67@gmail.com>
2022-12-16 19:20:40 -0600

    grid created for schedules page

--- lockerrm-front/src/components/WeeklySchedule.js
+++ lockerrm-front/src/components/WeeklySchedule.js
@@ -6,31 +6,50 @@ export default function WeeklySchedule(props) {
     let awayArr = props.awayDisplay;
     let gameArr = props.gameNameDisplay;
     let dateArr = props.dateDisplay;
-    let logoArr = props.logoDisplay;
-
-    
 
     const homeTeams = homeArr.map(homeTeam => <h3>{homeTeam}</h3>);
     const awayTeams = awayArr.map(awayTeam => <h3>{awayTeam}</h3>);
-    const gameName = gameArr.map(game => <div>{game}</div>);
-    const gameDate = dateArr.map(date => <div>{date}</div>);
-    const teamLogo = logoArr.map(logo => <div>{logo}</div>);
+    const gameName = gameArr.map(game => <h3>{game}</h3>);
+    const gameDate = dateArr.map(date => <h3>{date}</h3>);
+
+    const cardStyle = {
+        display: 'grid'
+    }
+    
+    const homeStyle = {
+        gridColumn: 1,
+        gridRow: 1,
+    }
+    
+    const gameNameStyle = {
+        gridColumn: 2,
+        gridRow: 1,
+    }
+
+    const awayStyle = {
+        gridColumn: 3,
+        gridRow: 1,
+    }
+
+    const gameDateStyle = {
+        gridColumn: 4,
+        gridRow: 1,
+    }
     
     return (
-        <div>
-            <div>
+        <div style={cardStyle}>
+            <div id='homeTeam' style={homeStyle}>
                 {homeTeams}
             </div>
-            <div >
+            <div id='gameName' style={gameNameStyle}>
                 {gameName}
             </div> 
-            <div>
+            <div id='awayTeam' style={awayStyle}>
                 {awayTeams}
             </div>
-            <div>
+            <div id='gameDate' style={gameDateStyle}>
                 {gameDate}
             </div>
         </div>
     )
-
 }
