54c63f46fb8d82b752e962c3da8fdb7e405a33af
Ryan Harris <ryan.t.harris67@gmail.com>
2022-12-16 18:49:41 -0600

    update

--- lockerrm-front/src/components/WeeklySchedule.js
+++ lockerrm-front/src/components/WeeklySchedule.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 
 export default function WeeklySchedule(props) {
 
@@ -8,77 +8,29 @@ export default function WeeklySchedule(props) {
     let dateArr = props.dateDisplay;
     let logoArr = props.logoDisplay;
 
+    
+
+    const homeTeams = homeArr.map(homeTeam => <h3>{homeTeam}</h3>);
+    const awayTeams = awayArr.map(awayTeam => <h3>{awayTeam}</h3>);
+    const gameName = gameArr.map(game => <div>{game}</div>);
+    const gameDate = dateArr.map(date => <div>{date}</div>);
+    const teamLogo = logoArr.map(logo => <div>{logo}</div>);
+    
     return (
-        '<div>' + homeArr + '</div>'
+        <div>
+            <div>
+                {homeTeams}
+            </div>
+            <div >
+                {gameName}
+            </div> 
+            <div>
+                {awayTeams}
+            </div>
+            <div>
+                {gameDate}
+            </div>
+        </div>
     )
 
-    
-    
 }
-
-    // const homeTeams = homeArr.map(homeTeam => <h3>{homeTeam}</h3>);
-    // const awayTeams = awayArr.map(awayTeam => <h3>{awayTeam}</h3>);
-    // const gameName = gameArr.map(game => <div>{game}</div>);
-    // const gameDate = dateArr.map(date => <div>{date}</div>);
-    // const teamLogo = logoArr.map(logo => <div>{logo}</div>);
-
-    // console.log(homeTeams);
-    // console.log(homeArr);
-
-    
-
-    // for(let i in homeArr) {
-    //     let homeTeam = homeArr;
-
-    //     for(let i in awayArr) {
-    //         let awayTeam = awayArr;
-
-    //         for(let i in gameArr) {
-    //             let gameName = gameArr;
-
-    //             for(let i in dateArr) {
-    //                 let gameDate = dateArr;
-
-    //                 for(let i in logoArr) {
-    //                     let teamLogo = logoArr;
-
-                        // gameCards = `<div class="gameCard">
-                        //                 <div>${homeTeam[i]}</div>
-                        //                 <div>${awayTeam[i]}</div>
-                        //                 <div>${gameName[i]}</div>
-                        //                 <div>${gameDate[i]}</div>
-                        //                 <img>${teamLogo[i]}</img>
-                        //             </div>`
-
-                        //             return (
-                        //                 <Fragment>{gameCards}</Fragment>
-                        //             )
-    //                 } 
-    //             }
-    //         }
-    //     }
-    // }
-
-
-
-
-
-    // let gameCard = "";
-
-    // for(let i in homeTeams) {
-    //     console.log(homeTeams[i].props.children)
-               
-    //     gameCard += `<div class="gameCard">
-    //                 <div>${homeTeams[i].props.children}</div>
-    //                 <div>${awayTeams}</div>
-    //                 <div>${gameName}</div>
-    //                 <div>${gameDate}</div>
-    //                 <img>${teamLogo}</img>
-    //             </div>`
-    // }
-
-//     return (
-//         // <Fragment>{gameCard}</Fragment> 
-//         console.log()      
-//     )
-// }
