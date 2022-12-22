eb1ee1b2a1cc20406305f3043ea6c87c2b959bd0
CleoBreaux <corey.osbey@gmail.com>
2022-12-22 17:28:04 -0600

    news api added

--- lockerrm-front/src/components/Home.js
+++ lockerrm-front/src/components/Home.js
@@ -1,6 +1,21 @@
 import {useNavigate} from "react-router-dom";
 
 function Home () {
+
+    const options = {
+        method: 'GET',
+        headers: {
+            source: '3',
+            'X-RapidAPI-Key': process.env.newsApiKey,
+            'X-RapidAPI-Host': 'nfl-news-api.p.rapidapi.com'
+        }
+    };
+
+    fetch('https://nfl-news-api.p.rapidapi.com/SourceNews?source=1', options)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+
     return (
         <div className="Home">
             <h1>This is the [ Home page]</h1>
