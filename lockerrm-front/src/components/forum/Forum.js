24f3208227a9f1c4b938446fe615b104d5a78c31
Vicente <figueroa.vicente9@gmail.com>
2022-12-29 19:07:00 -0600

    refactored forum component and created template for editing post

--- lockerrm-front/src/components/forum/Forum.js
+++ lockerrm-front/src/components/forum/Forum.js
@@ -3,16 +3,17 @@ import { useNavigate } from 'react-router-dom';
 import authService from "../../services/auth.service";
 
 const Forum = () => {
-
-  const API = "http://localhost:8080/posts";
-  // grabs the user from local storage from auth.service func.
-  authService.getCurrentUser();
-  //           traverse object for key
-  // console.log(authService.getCurrentUser()['username']);
-
-
+  
   const [posts, setPosts] = useState([]);
   const navigate = useNavigate();
+  const API = "http://localhost:8080/posts";
+  const options = {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("access_token")
+      },
+    }
   
   // useEffect runs on initial page load and everytime the data changes
   useEffect(() => {
@@ -21,14 +22,23 @@ const Forum = () => {
 
   const getAllPosts = async () => {
     let allPosts = [];
-    const response = fetch(API);
-    const data = (await response).json();
-    allPosts.push(data);
-    console.log(data);
+
+    fetch(API, options)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      allPosts.push(data);
+    });
+
     setPosts(allPosts);
   }
 
   console.log(posts);
+  
+  // Create function for edit button
+
+
+  // Create function to delete post.
   
 
   return (
@@ -45,14 +55,17 @@ const Forum = () => {
         <option value="all-posts">All Posts</option>
       </select>
 
-      {/* <div className="allPosts">
+      <div className="allPosts">
+        {/* Display all posts here */}
         {posts.map((post) => (
           <div key={post.id}>
               <small>Created by @{post.user.username}</small>
               <p>{post.postBody}</p>
+              <button>Edit</button>
+              <button>Delete</button>
           </div>
         ))}
-      </div> */}
+      </div>
 
     </div>
   );
