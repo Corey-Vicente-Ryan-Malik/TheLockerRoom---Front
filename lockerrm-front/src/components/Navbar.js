a541d5f309e9eacd523f31653bc1607a346d374c
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-01-03 09:07:07 -0600

    corrected errors with merge

--- lockerrm-front/src/components/Navbar.js
+++ lockerrm-front/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, { Component, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import GameDisplay from '../displayFolder/GameDisplay';
 import TeamDisplay from '../displayFolder/TeamDisplay';
@@ -9,19 +9,18 @@ import Register from '../settings/Register';
 import CreatePost from '../forum/CreatePost';
 import EditPost from '../forum/EditPost';
 import Forum from '../forum/Forum';
-import authService from "../services/auth.service";
-import About from "./About";
-import Error from "../settings/Error";
+import authService from '../services/auth.service';
+import About from './About';
+import Error from '../settings/Error';
 
-export default function Navbar(){
-  const currentUser = authService.getCurrentUser()
-
+export default function Navbar() {
+  const currentUser = authService.getCurrentUser();
 
   const [team, setTeam] = useState({
-    favoriteTeam: "",
+    favoriteTeam: '',
   });
 
-  const {favoriteTeam} = team;
+  const { favoriteTeam } = team;
   console.log(team);
   const onInputChange = (e) => {
     setTeam({ ...team, [e.target.name]: e.target.value });
@@ -30,9 +29,8 @@ export default function Navbar(){
   const logOut = (e) => {
     e.preventDefault();
     authService.logout();
-  }
-    return(
-     
+  };
+  return (
     <React.Fragment>
       <Router>
         <div className="Navbar">
@@ -56,120 +54,137 @@ export default function Navbar(){
               id="navbarSupportedContent"
             >
               <ul className="navbar-nav mr-auto">
+                {currentUser && (
+                  <li className="nav-item">
+                    <Link to={'/forum'} id="forum" className="nav-link">
+                      Forum
+                    </Link>
+                  </li>
+                )}
 
+                <li className="nav-item">
+                  <Link to={'/teams'} id="teams" className="nav-link">
+                    Team Information
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/schedules'} id="schedules" className="nav-link">
+                    Weekly Schedules
+                  </Link>
+                </li>
+              </ul>
+              <div className="d-flex my-2 my-lg-0 ms-auto">
+                <select name="favoriteTeam" onChange={(e) => onInputChange(e)}>
+                  {/*<option defaultValue="Select Your Favorite Team">*/}
+                  {/*  Select Your Favorite Team*/}
+                  {/*</option>*/}
+                  <option value="San Fransisco 49ers">
+                    San Fransisco 49ers
+                  </option>
+                  <option value="Chicago Bears">Chicago Bears</option>
+                  <option value="Cincinnati Bengals">Cincinnati Bengals</option>
+                  <option value="Buffalo Bills">Buffalo Bills</option>
+                  <option value="Denver Broncos">Denver Broncos</option>
+                  <option value="Cleveland Browns">Cleveland Browns</option>
+                  <option value="Tampa Bay Buccaneers">
+                    Tampa Bay Buccaneers
+                  </option>
+                  <option value="Arizona Cardinals">Arizona Cardinals</option>
+                  <option value="Los Angeles Chargers">
+                    Los Angeles Chargers
+                  </option>
+                  <option value="Kansas City Chiefs">Kansas City Chiefs</option>
+                  <option value="Indianapolis Colts">Indianapolis Colts</option>
+                  <option value="Washington Commanders">
+                    Washington Commanders
+                  </option>
+                  <option value="Dallas Cowboys">Dallas Cowboys</option>
+                  <option value="Miami Dolphins">Miami Dolphins</option>
+                  <option value="Philadelphia Eagles">
+                    Philadelphia Eagles
+                  </option>
+                  <option value="Atlanta Falcons">Atlanta Falcons</option>
+                  <option value="New York Giants">New York Giants</option>
+                  <option value="Jacksonville Jaguars">
+                    Jacksonville Jaguars
+                  </option>
+                  <option value="New York Jets">New York Jets</option>
+                  <option value="Detroit Lions">Detroit Lions</option>
+                  <option value="Green Bay Packers">Green Bay Packers</option>
+                  <option value="Carolina Panthers">Carolina Panthers</option>
+                  <option value="New England Patriots">
+                    New England Patriots
+                  </option>
+                  <option value="Las Vegas Raiders">Las Vegas Raiders</option>
+                  <option value="Los Angeles Rams">Los Angeles Rams</option>
+                  <option value="Baltimore ravens">Baltimore Ravens</option>
+                  <option value="New Orleans Saints">New Orleans Saints</option>
+                  <option value="Seattle Seahawks">Seattle Seahawks</option>
+                  <option value="Pittsburgh Steelers">
+                    {' '}
+                    Pittsburgh Steelers
+                  </option>
+                  <option value="Houston Texans">Houston Texans</option>
+                  <option value="Tennessee Titans">Tennessee Titans</option>
+                  <option value="Minnesota Vikings">Minnesota Vikings</option>
+                </select>
+              </div>
+
+              <ul className="navbar-nav">
+                <li className="nav-item dropdown">
+                  <Link
+                    to={'/lockerrm-front/src/components/Navbar#'}
+                    id="navbarDropdownMenuLink"
+                    className="nav-link dropdown-toggle"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <i className="fa-solid fa-user-gear"></i>
+                  </Link>
+                  <ul
+                    className="dropdown-menu dropdown-menu-end"
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
                     {currentUser && (
-                        <li className="nav-item">
-                          <Link to={'/forum'} id="forum" className="nav-link">
-                            Forum
-                          </Link>
-                        </li>
+                      <li>
+                        <Link to={'/edit'} id="edit" className="dropdown-item">
+                          Edit Profile
+                        </Link>
+                      </li>
                     )}
-
-                    <li className="nav-item">
-                      <Link to={'/teams'} id="teams" className="nav-link">
-                        Team Information
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link to={'/schedules'} id="schedules" className="nav-link">
-                        Weekly Schedules
-                      </Link>
-                    </li>
+                    {currentUser && (
+                      <li>
+                        <Link
+                          to={'/Settings'}
+                          id="settings"
+                          className="dropdown-item"
+                        >
+                          Settings
+                        </Link>
+                      </li>
+                    )}
+                    {currentUser ? (
+                      <li>
+                        <Link
+                          to={'/'}
+                          className="dropdown-item"
+                          onClick={logOut}
+                        >
+                          Logout
+                        </Link>
+                      </li>
+                    ) : (
+                      <li>
+                        <Link to={'/'} className="dropdown-item">
+                          Login
+                        </Link>
+                      </li>
+                    )}
                   </ul>
-                  <div className="d-flex my-2 my-lg-0 ms-auto">
-                    <select
-                        name="favoriteTeam"
-                        onChange={(e) => onInputChange(e)}
-                    >
-                      {/*<option defaultValue="Select Your Favorite Team">*/}
-                      {/*  Select Your Favorite Team*/}
-                      {/*</option>*/}
-                      <option value="San Fransisco 49ers">San Fransisco 49ers</option>
-                      <option value="Chicago Bears">Chicago Bears</option>
-                      <option value="Cincinnati Bengals">Cincinnati Bengals</option>
-                      <option value="Buffalo Bills">Buffalo Bills</option>
-                      <option value="Denver Broncos">Denver Broncos</option>
-                      <option value="Cleveland Browns">Cleveland Browns</option>
-                      <option value="Tampa Bay Buccaneers">Tampa Bay Buccaneers</option>
-                      <option value="Arizona Cardinals">Arizona Cardinals</option>
-                      <option value="Los Angeles Chargers">Los Angeles Chargers</option>
-                      <option value="Kansas City Chiefs">Kansas City Chiefs</option>
-                      <option value="Indianapolis Colts">Indianapolis Colts</option>
-                      <option value="Washington Commanders">Washington Commanders</option>
-                      <option value="Dallas Cowboys">Dallas Cowboys</option>
-                      <option value="Miami Dolphins">Miami Dolphins</option>
-                      <option value="Philadelphia Eagles">Philadelphia Eagles</option>
-                      <option value="Atlanta Falcons">Atlanta Falcons</option>
-                      <option value="New York Giants">New York Giants</option>
-                      <option value="Jacksonville Jaguars">Jacksonville Jaguars</option>
-                      <option value="New York Jets">New York Jets</option>
-                      <option value="Detroit Lions">Detroit Lions</option>
-                      <option value="Green Bay Packers">Green Bay Packers</option>
-                      <option value="Carolina Panthers">Carolina Panthers</option>
-                      <option value="New England Patriots">New England Patriots</option>
-                      <option value="Las Vegas Raiders">Las Vegas Raiders</option>
-                      <option value="Los Angeles Rams">Los Angeles Rams</option>
-                      <option value="Baltimore ravens">Baltimore Ravens</option>
-                      <option value="New Orleans Saints">New Orleans Saints</option>
-                      <option value="Seattle Seahawks">Seattle Seahawks</option>
-                      <option value="Pittsburgh Steelers"> Pittsburgh Steelers</option>
-                      <option value="Houston Texans">Houston Texans</option>
-                      <option value="Tennessee Titans">Tennessee Titans</option>
-                      <option value="Minnesota Vikings">Minnesota Vikings</option>
-                    </select>
-                  </div>
-
-                  <ul className="navbar-nav">
-                    <li className="nav-item dropdown">
-                      <Link
-                          to={'/lockerrm-front/src/components/Navbar#'}
-                          id="navbarDropdownMenuLink"
-                          className="nav-link dropdown-toggle"
-                          role="button"
-                          data-bs-toggle="dropdown"
-                          aria-expanded="false"
-                      >
-                        <i className="fa-solid fa-user-gear"></i>
-                      </Link>
-                      <ul
-                          className="dropdown-menu dropdown-menu-end"
-                          aria-labelledby="navbarDropdownMenuLink"
-                      >
-                        {currentUser &&(
-                            <li>
-                              <Link to={"/edit"} id="edit" className="dropdown-item">
-                                Edit Profile
-                              </Link>
-                            </li>
-                        )}
-                        {currentUser && (
-                            <li>
-                              <Link to={"/Settings"} id="settings" className="dropdown-item">
-                                Settings
-                              </Link>
-                            </li>
-                        )}
-                        {currentUser ? (
-                            <li>
-                              <Link to={"/"} className="dropdown-item" onClick={logOut}>
-                                Logout
-                              </Link>
-                            </li>
-                        ):(
-                            <li>
-                              <Link to={"/"} className="dropdown-item">
-                                Login
-                              </Link>
-                            </li>
-                        )}
-                      </ul>
-                    </li>
-                  </ul>
-
-                </div>
-              </nav>
+                </li>
+              </ul>
             </div>
-
           </nav>
         </div>
         <Routes>
@@ -184,11 +199,9 @@ export default function Navbar(){
           <Route path="/create-post" element={<CreatePost />} />
           <Route path="/edit-post" element={<EditPost />} />
           <Route path="/about" element={<About />} />
-          <Route path='*' element={<Error />}/>
+          <Route path="*" element={<Error />} />
         </Routes>
       </Router>
     </React.Fragment>
   );
-
 }
-
