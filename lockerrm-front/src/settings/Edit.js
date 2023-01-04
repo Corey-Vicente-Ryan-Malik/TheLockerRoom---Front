904468cb42644617b4e0aeb686526146432bdc6a
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-01-04 11:51:54 -0600

    branch commit

--- lockerrm-front/src/settings/Edit.js
+++ lockerrm-front/src/settings/Edit.js
@@ -1,230 +1,202 @@
-import React from "react";
-import authService from "../services/auth.service";
-function Edit() {
-  const currentUser = authService.getCurrentUser();
+import React, { useState } from 'react';
+import authService from '../services/auth.service';
+
+export default function Edit() {
+  const [username, setUsername] = useState('');
+  // const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [favTeam, setFavTeam] = useState('');
+
+  const loggedInUser = authService.getCurrentUser();
+  // console.log(loggedInUser);
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  // const handlePasswordChange = (e) => {
+  //   setPassword(e.target.value);
+  // };
+  const handleFirstChange = (e) => {
+    setFirstName(e.target.value);
+  };
+  const handleLastChange = (e) => {
+    setLastName(e.target.value);
+  };
+  const handleFavTeamChange = (e) => {
+    setFavTeam(e.target.value);
+  };
+  const submitChange = () => {
+    let User = {
+      id: loggedInUser.id,
+      role: loggedInUser.role,
+      email: loggedInUser.email,
+      firstName: firstName,
+      lastName: lastName,
+      username: username,
+      // password: password,
+      favoriteTeam: favTeam,
+    };
+    // console.log(User);
+    const API = `http://localhost:8080/users/${loggedInUser.id}/edit-profile`;
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+      },
+      body: JSON.stringify(User),
+    };
+    fetch(API, options).then((response) => console.log(response.data));
+  };
+
+  const edit = {
+    display: 'grid',
+    width: '50%',
+    height: '50%',
+    margin: 'auto',
+    gridColumn: 1,
+    gridRow: 1,
+    textAlign: 'center',
+  };
+
   return (
-    <div className="Edit" style={EditPage}>
-      <div className="Edit form" style={EditForm}>
-        <form className="editForm">
-          <input
-            style={firstName}
-            type="text"
-            placeholder="First Name"
-            name="firstName"
-            value={currentUser["firstName"]}
-          />
-
-          <input
-            style={lastName}
-            type="text"
-            placeholder=" Last Name"
-            name="lastName"
-            value={currentUser["lastName"]}
-          />
-
-          <input style={Email} type="text" placeholder="Email" name="email" value={currentUser["email"]}/>
-
-          <input
-            style={Username}
-            type="text"
-            placeholder="Username"
-            name="username"
-            value={currentUser["username"]}
-          />
-
-          <select name="favorite team" style={FavoriteTeam} value={currentUser["favTeam"]}>
-            <option defaultValue="Select Your Favorite Team">
-              Select Your Favorite Team
-            </option>
-            <option value="San Fransisco 49ers">San Fransisco 49ers</option>
-            <option value="Chicago Bears">Chicago Bears</option>
-            <option value="Cincinnati Bengals">Cincinnati Bengals</option>
-            <option value="Buffalo Bills">Buffalo Bills</option>
-            <option value="Denver Broncos">Denver Broncos</option>
-            <option value="Cleveland Browns">Cleveland Browns</option>
-            <option value="Tampa Bay Buccaneers">Tampa Bay Buccaneers</option>
-            <option value="Arizona Cardinals">Arizona Cardinals</option>
-            <option value="Los Angeles Chargers">Los Angeles Chargers</option>
-            <option value="Kansas City Chiefs">Kansas City Chiefs</option>
-            <option value="Indianapolis Colts">Indianapolis Colts</option>
-            <option value="Washington Commanders">Washington Commanders</option>
-            <option value="Dallas Cowboys">Dallas Cowboys</option>
-            <option value="Miami Dolphins">Miami Dolphins</option>
-            <option value="Philadelphia Eagles">Philadelphia Eagles</option>
-            <option value="Atlanta Falcons">Atlanta Falcons</option>
-            <option value="New York Giants">New York Giants</option>
-            <option value="Jacksonville Jaguars">Jacksonville Jaguars</option>
-            <option value="New York Jets">New York Jets</option>
-            <option value="Detroit Lions">Detroit Lions</option>
-            <option value="Green Bay Packers">Green Bay Packers</option>
-            <option value="Carolina Panthers">Carolina Panthers</option>
-            <option value="New England Patriots">New England Patriots</option>
-            <option value="Las Vegas Raiders">Las Vegas Raiders</option>
-            <option value="Los Angeles Rams">Los Angeles Rams</option>
-            <option value="Baltimore ravens">Baltimore Ravens</option>
-            <option value="New Orleans Saints">New Orleans Saints</option>
-            <option value="Seattle Seahawks">Seattle Seahawks</option>
-            <option value="Pittsburgh Steelers"> Pittsburgh Steelers</option>
-            <option value="Houston Texans">Houston Texans</option>
-            <option value="Tennessee Titans">Tennessee Titans</option>
-            <option value="Minnesota Vikings">Minnesota Vikings</option>
-          </select>
-
-          <p style={Passmessage}>Enter your password to confirm your changes</p>
-
-          <input
-            style={Password}
-            placeholder="Password"
-            type="password"
-            name="password"
-
-          />
-
-          <input
-            style={ConfirmPass}
-            placeholder=" Confirm Password"
-            type="password"
-            name=" confirm password"
-          />
-
-          <button style={SaveButton} type="sumbit" name="Save changes">
-            Save Changes
-          </button>
-        </form>
-      </div>
+    <div style={edit}>
+      <h1>Hello New Edit Form</h1>
+      <input
+        type="text"
+        id="firstName"
+        name="firstName"
+        placeholder="First Name"
+        onBlur={handleFirstChange}
+      />
+      <br />
+      <input
+        type="text"
+        id="lastName"
+        name="lastName"
+        placeholder="Last Name"
+        onBlur={handleLastChange}
+      />
+      <br />
+      <input
+        type="text"
+        id="username"
+        name="username"
+        placeholder="Username"
+        onBlur={handleUsernameChange}
+      />
+      <br />
+      {/* <input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Password"
+        onBlur={handlePasswordChange}
+      />
+      <br /> */}
+      <select name="favoriteTeam" onBlur={handleFavTeamChange}>
+        <option name="favoriteOption">Choose Your Favorite Team</option>
+        <option name="favoriteOption" value="1">
+          Atlanta Falcons
+        </option>
+        <option name="favoriteOption" value="2">
+          Buffalo Bills
+        </option>
+        <option name="favoriteOption" value="3">
+          Chicago Bears
+        </option>
+        <option name="favoriteOption" value="4">
+          Cincinnati Bengals
+        </option>
+        <option name="favoriteOption" value="5">
+          Cleveland Browns
+        </option>
+        <option name="favoriteOption" value="6">
+          Dallas Cowboys
+        </option>
+        <option name="favoriteOption" value="7">
+          Denver Broncos
+        </option>
+        <option name="favoriteOption" value="8">
+          Detroit Lions
+        </option>
+        <option name="favoriteOption" value="9">
+          Green Bay Packers
+        </option>
+        <option name="favoriteOption" value="10">
+          Tennessee Titans
+        </option>
+        <option name="favoriteOption" value="11">
+          Indianapolis Colts
+        </option>
+        <option name="favoriteOption" value="12">
+          Kansas City Chiefs
+        </option>
+        <option name="favoriteOption" value="13">
+          Las Vegas Raiders
+        </option>
+        <option name="favoriteOption" value="14">
+          Los Angeles Rams
+        </option>
+        <option name="favoriteOption" value="15">
+          Miami Dolphins
+        </option>
+        <option name="favoriteOption" value="16">
+          Minnesota Vikings
+        </option>
+        <option name="favoriteOption" value="17">
+          New England Patriots
+        </option>
+        <option name="favoriteOption" value="18">
+          New Orleans Saints
+        </option>
+        <option name="favoriteOption" value="19">
+          New York Giants
+        </option>
+        <option name="favoriteOption" value="20">
+          New York Jets
+        </option>
+        <option name="favoriteOption" value="21">
+          Philadelphia Eagles
+        </option>
+        <option name="favoriteOption" value="22">
+          Arizona Cardinals
+        </option>
+        <option name="favoriteOption" value="23">
+          Pittsburgh Steelers
+        </option>
+        <option name="favoriteOption" value="24">
+          Los Angeles Chargers
+        </option>
+        <option name="favoriteOption" value="25">
+          San Fransisco 49ers
+        </option>
+        <option name="favoriteOption" value="26">
+          Seattle Seahawks
+        </option>
+        <option name="favoriteOption" value="27">
+          Tampa Bay Buccaneers
+        </option>
+        <option name="favoriteOption" value="28">
+          Washington Commanders
+        </option>
+        <option name="favoriteOption" value="29">
+          Carolina Panthers
+        </option>
+        <option name="favoriteOption" value="30">
+          Jacksonville Jaguars
+        </option>
+        <option name="favoriteOption" value="33">
+          Baltimore Ravens
+        </option>
+        <option name="favoriteOption" value="34">
+          Houston Texans
+        </option>
+      </select>
+      <br />
+      <button onClick={submitChange}>Submit Changes</button>
+      <br />
     </div>
   );
 }
-
-const EditPage = {
-  display: 'flex',
-  margin: 'auto',
-  width: '95vw',
-  height: '95vh',
-};
-
-const firstName = {
-  gridColumn: 1,
-  gridRow: 1,
-  margin: '10px',
-  width: '90%',
-  display: 'flex',
-  justifyContent: 'center',
-  justifyItems: 'center',
-  alignItems: 'center',
-  padding: '5px',
-  borderRadius: '10px',
-};
-
-const lastName = {
-  gridColumn: 1,
-  gridRow: 2,
-  margin: '10px',
-  width: '90%',
-  display: 'flex',
-  justifyContent: 'center',
-  justifyItems: 'center',
-  alignItems: 'center',
-  padding: '5px',
-  borderRadius: '10px',
-};
-
-const Email = {
-  gridColumn: 1,
-  gridRow: 3,
-  margin: '10px',
-  width: '90%',
-  display: 'flex',
-  justifyContent: 'center',
-  justifyItems: 'center',
-  alignItems: 'center',
-  padding: '5px',
-  borderRadius: '10px',
-};
-
-const Username = {
-  gridColumn: 1,
-  gridRow: 4,
-  margin: '10px',
-  width: '90%',
-  display: 'flex',
-  justifyContent: 'center',
-  justifyItems: 'center',
-  alignItems: 'center',
-  padding: '5px',
-  borderRadius: '10px',
-};
-
-const FavoriteTeam = {
-  gridColumn: 1,
-  gridRow: 5,
-  margin: '10px',
-  width: '90%',
-  display: 'flex',
-  justifyContent: 'center',
-  justifyItems: 'center',
-  alignItems: 'center',
-  padding: '5px',
-  borderRadius: '10px',
-};
-
-const Passmessage = {
-  gridColumn: 1,
-  gridRow: 6,
-  margin: '10px',
-};
-
-const Password = {
-  gridColumn: 1,
-  gridRow: 7,
-  margin: '10px',
-  width: '90%',
-  display: 'flex',
-  justifyContent: 'center',
-  justifyItems: 'center',
-  alignItems: 'center',
-  padding: '5px',
-  borderRadius: '10px',
-};
-
-const ConfirmPass = {
-  gridColumn: 1,
-  gridRow: 8,
-  margin: '10px',
-  width: '90%',
-  display: 'flex',
-  justifyContent: 'center',
-  justifyItems: 'center',
-  alignItems: 'center',
-  padding: '5px',
-  border: 'solid black',
-  borderRadius: '15px',
-};
-
-const SaveButton = {
-  gridColumn: 1,
-  gridRow: 9,
-  margin: '10px',
-  width: '60%',
-  display: 'flex',
-  justifyContent: 'center',
-  justifyItems: 'center',
-  alignItems: 'center',
-  padding: '5px',
-  borderRadius: '10px',
-};
-
-const EditForm = {
-  display: 'grid',
-  margin: 'auto',
-  border: 'solid black',
-  borderRadius: '15px',
-  // width: '60%',
-  // height: '70%',
-};
-
-const message = {};
-
-const select = {};
-
-export default Edit;
