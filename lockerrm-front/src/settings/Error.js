b841f779c7c3be8d791b559e91d259c0411eb519
CleoBreaux <corey.osbey@gmail.com>
2023-01-04 16:58:44 -0600

    error page updated

--- lockerrm-front/src/settings/Error.js
+++ lockerrm-front/src/settings/Error.js
@@ -3,8 +3,8 @@ import React from "react";
 
 function Error() {
     return(
-        <div>
-            <img src={holup}/>
+        <div className="ErrorPage" style={ErrorPage}>
+            <img style={Image} src={holup}/>
 
             <h1>
                 404...That page doesn't exist big fella.
@@ -12,6 +12,22 @@ function Error() {
         </div>
 
     )
+}
+
+const ErrorPage = {
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center',
+    // justifyContent: 'center',
+    margin:'15px auto'
+
+
+}
+
+const Image = {
+    height:'50%',
+    width:'50%'
+
 }
 
 export default Error
