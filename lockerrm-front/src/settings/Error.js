ef60929627584b4d022effd1661423554505c555
Ryan Harris <109987136+ryant67@users.noreply.github.com>
2023-01-05 12:23:36 -0600

    fixed error

--- lockerrm-front/src/settings/Error.js
+++ lockerrm-front/src/settings/Error.js
@@ -1,33 +1,25 @@
-import holup from '../images/holup.jpeg'
-import React from "react";
+import holup from '../images/holup.jpeg';
+import React from 'react';
 
-function Error() {
-    return(
-        <div className="ErrorPage" style={ErrorPage}>
-            <img style={Image} src={holup}/>
+export default function Error() {
+  return (
+    <div className="ErrorPage" style={ErrorPage}>
+      <img style={Image} src={holup} />
 
-
-            <h1>
-                404...That page doesn't exist big fella.
-            </h1>
-        </div>
-
-    )
+      <h1>404...That page doesn't exist big fella.</h1>
+    </div>
+  );
 }
 
 const ErrorPage = {
-    display:'flex',
-    flexDirection:'column',
-    alignItems:'center',
-    // justifyContent: 'center',
-    margin:'15px auto'
-
-
-}
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  // justifyContent: 'center',
+  margin: '15px auto',
+};
 
 const Image = {
-    height:'50%',
-    width:'50%'
-
-}
-
+  height: '50%',
+  width: '50%',
+};
