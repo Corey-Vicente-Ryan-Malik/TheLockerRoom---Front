bda151bc0c1511bce9ef74a9e48c023f48b6716e
Vicente <figueroa.vicente9@gmail.com>
2023-01-05 23:21:00 -0600

    refactored forums page

--- lockerrm-front/src/forum/Forum.js
+++ lockerrm-front/src/forum/Forum.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import authService from "../services/auth.service";
-import "./Forum.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
-
+import Dropdown from "react-bootstrap/Dropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Forum.css";
 
 const Forum = () => {
   const [posts, setPosts] = useState([]);
@@ -35,12 +36,25 @@ const Forum = () => {
       setPosts(data);
     });
   }
+
+  const customToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <a
+      href=""
+      ref={ref}
+      onClick={e => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+      {children}
+      <span className="threeDots" />
+    </a>
+  ));
   
   return (
-    <div>
+    <div style={{ backgroundColor: "e5e4e2" }}>
       <h2 className="forumWelcome">The Locker Room Forums</h2>
 
-      <Button style={{marginLeft: '19rem', marginBottom: '1rem'}} variant="dark" onClick={() => navigate('/create-post')}>Create Post</Button>
 
       <Container>
         <Alert variant="success">
@@ -54,18 +68,23 @@ const Forum = () => {
             Have any questions or concerns? Contact us at lockerrm@gmail.com
           </p>
         </Alert>
-        <Row>
-          {posts.map((post) => (
-            <Col>
-              <Card style={{
-                width: '20rem',
-                padding: '1rem',
-                marginBottom: '1rem',
-                }} key={post.id}>
-                  <Card.Subtitle className="text-muted">Created by @{post.user.username}</Card.Subtitle>
-                  <Card.Text>{post.postBody}</Card.Text>
 
-                  <Button style={{width: '30%'}} variant="outline-success" size="sm" onClick={() => {
+        <Button style={{marginBottom: '1rem'}} variant="dark" onClick={() => navigate('/create-post')}>Create Post</Button>
+
+        {posts.map((post) => (
+          <Card style={{
+            width: '80%',
+            padding: '1.5rem',
+            marginBottom: '1rem',
+            }} key={post.id}>
+
+              <Card.Subtitle className="text-muted">Posted by {post.user.username}</Card.Subtitle>
+              <Card.Text style={{ marginBottom: "0px"}}>{post.postBody}</Card.Text>
+
+              <Dropdown>
+                <Dropdown.Toggle as={customToggle} />
+                <Dropdown.Menu size="sm" title="">
+                  <Dropdown.Item onClick={() => {
                     // If logged in user username === posts username
                     // Send user to edit post form
                     if (currentUser["username"] === post.user.username) {
@@ -73,9 +92,8 @@ const Forum = () => {
                     } else {
                       alert("You are not the owner of the post.");
                     }
-                  }}>Edit</Button>
-
-                  <Button style={{width: '30%', }} variant="outline-danger" size="sm" onClick={() => {
+                  }}>Edit</Dropdown.Item>
+                  <Dropdown.Item onClick={() => {
                     // If logged in user username === posts username
                     // Allow user to delete post
                     if (currentUser["username"] === post.user.username) {
@@ -92,11 +110,11 @@ const Forum = () => {
                     } else {
                       alert("You are not the owner of this post.");
                     }
-                  }}>Delete</Button>
-              </Card>
-            </Col>
-          ))}
-        </Row>
+                  }}>Delete</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+          </Card>
+        ))}
       </Container>
 
     </div>
