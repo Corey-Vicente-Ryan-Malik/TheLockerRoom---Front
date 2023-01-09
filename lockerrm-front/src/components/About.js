d38e2ee0caeb54ca567c7f0fb39a6986c45191ff
CleoBreaux <corey.osbey@gmail.com>
2023-01-09 17:26:48 -0600

    about page updated

--- lockerrm-front/src/components/About.js
+++ lockerrm-front/src/components/About.js
@@ -2,6 +2,8 @@ import about1 from '../images/CO.jpg';
 import about2 from '../images/RH.jpeg';
 import about3 from '../images/MW.JPG';
 import about4 from '../images/VF2.jpeg';
+import icon1 from '../images/codeup.jpg';
+import '../displayFolder/About.css'
 import React from "react";
 import {Button, Card} from "react-bootstrap";
 
@@ -9,7 +11,7 @@ export default function About() {
 
     return (
 
-        <div style={aboutPage}>
+        <div id={aboutPage} style={aboutPage}>
             <div style={aboutContent}>
                 <h1>Team United. Strength in Unity !</h1>
                 <p>
@@ -25,56 +27,56 @@ export default function About() {
 
             <h1>Meet The Developers!</h1>
 
-            <div className="Developer" style={Developer}>
+            <div id="Developer" className="Developer" style={Developer}>
 
 
-                <Card style={{width: '18rem', margin: '15px'}}>
+                <Card style={devCard} id="devCard">
                     <Card.Img variant="top" src={about1}/>
                     <Card.Body>
                         <Card.Title>Corey Osbey</Card.Title>
-                        <Card.Text>
+                        <Card.Text style={cardIcon}>
                             <a target={"_blank"} href="https://www.linkedin.com/in/coreyosbey/"> <i style={icon} className="fa-brands fa-linkedin"></i></a>
                             <a target={"_blank"} href="https://github.com/Cosbey1 "><i style={icon} className="fa-brands fa-github"></i></a>
-                            <a target={"_blank"} href="https://alumni.codeup.com/students/1669"><i style={icon} className="fa-solid fa-envelope"></i></a>
+                            <a target={"_blank"} href="https://alumni.codeup.com/students/1669"><i style={icon}><img style={codeLogo} src={icon1}/></i></a>
                         </Card.Text>
 
                     </Card.Body>
                 </Card>
 
-                <Card style={{width: '18rem', margin: '15px'}}>
+                <Card style={devCard}>
                     <Card.Img variant="top" src={about2}/>
                     <Card.Body>
                         <Card.Title>Ryan Harris</Card.Title>
-                        <Card.Text >
+                        <Card.Text style={cardIcon}>
                             <a target={"_blank"} href="https://www.linkedin.com/in/ryantharris/ "><i style={icon} className="fa-brands fa-linkedin"></i></a>
                             <a target={"_blank"} href="https://github.com/ryant67 "><i style={icon} className="fa-brands fa-github"></i></a>
-                            <a target={"_blank"} href="https://alumni.codeup.com/students/1657"><i style={icon} className="fa-solid fa-envelope"></i></a>
+                            <a target={"_blank"} href="https://alumni.codeup.com/students/1657"><i style={icon}><img style={codeLogo} src={icon1}/></i></a>
                         </Card.Text>
 
                     </Card.Body>
                 </Card>
 
-                <Card style={{width: '18rem', margin: '15px'}}>
+                <Card style={devCard}>
                     <Card.Img variant="top" src={about3}/>
                     <Card.Body>
                         <Card.Title>Malik Willis</Card.Title>
-                        <Card.Text>
+                        <Card.Text style={cardIcon}>
                             <a target={"_blank"} href="https://www.linkedin.com/in/malik-willis-71778924a/"><i style={icon} className="fa-brands fa-linkedin"></i></a>
                             <a target={"_blank"} href="https://github.com/Leek995"><i style={icon} className="fa-brands fa-github"></i></a>
-                            <a target={"_blank"} href="https:// "><i style={icon} className="fa-solid fa-envelope"></i></a>
+                            <a target={"_blank"} href="https://alumni.codeup.com/students/1660"><i style={icon}><img style={codeLogo} src={icon1}/></i></a>
                         </Card.Text>
 
                     </Card.Body>
                 </Card>
 
-                <Card style={{width: '18rem', margin: '15px'}}>
+                <Card style={devCard}>
                     <Card.Img variant="top" src={about4}/>
                     <Card.Body>
                         <Card.Title>Vicente Figueroa</Card.Title>
-                        <Card.Text>
+                        <Card.Text style={cardIcon}>
                             <a target={"_blank"} href="https://www.linkedin.com/in/vicentefigueroa-dev/"><i style={icon} className="fa-brands fa-linkedin"></i></a>
                             <a target={"_blank"} href="https://github.com/vicentefiguer0a"><i style={icon} className="fa-brands fa-github"></i></a>
-                            <a target={"_blank"} href="https://alumni.codeup.com/students/1664"><i style={icon} className="fa-solid fa-envelope"></i></a>
+                            <a target={"_blank"} href="https://alumni.codeup.com/students/1664"><i style={icon}><img style={codeLogo} src={icon1}/></i></a>
                         </Card.Text>
 
                     </Card.Body>
@@ -88,7 +90,9 @@ export default function About() {
 
 const aboutPage = {
     display: 'grid',
-    width: '100%'
+    margin:'15px'
+    // width: '50%'
+    // width:'50vh'
 }
 const aboutContent = {
     gridColumn: 1,
@@ -100,14 +104,32 @@ const aboutContent = {
 
 const Developer = {
     display: 'flex',
-    alignItems: 'center'
+    alignItems: 'center',
+    margin:'auto'
+    // justifyContent: 'center',
+}
 
+const devCard = {
+    width: '18vw',
+    margin: '15px'
 }
 
 const icon = {
-    margin: '5px',
+    margin: '15px',
     fontSize: '25px'
 
+}
+
+const cardIcon = {
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center'
+
+}
+
+const codeLogo = {
+    width: '30px',
+    height:'30px'
 }
 
 // const iconDisplay = {
